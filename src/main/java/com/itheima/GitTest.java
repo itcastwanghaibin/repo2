3b0548391f1010bbd3d9cf64306c52ac8b62edcd
itheima <1718688348@qq.com>
2021-05-29 16:34:57 +0800

    第一次添加本地工程到git

---
+++ src/main/java/com/itheima/GitTest.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+        System.out.println("11111111111111111");
+    }
+}
